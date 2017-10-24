4c9dd2c52b4b39d6bdfb9032539e55ab445e5040
Tijmen <tijmen_222@hotmail.com>
2017-10-24 19:19:23 +0200

    Windows patch, fix #8

--- collector/fileutils.cpp
+++ collector/fileutils.cpp
@@ -1,12 +1,62 @@
 #include "fileutils.hpp"
+#if defined _WIN32
+#include <Windows.h>
 
+#elif defined __linux
 #include <sys/types.h> // include before stat.h
 #include <sys/stat.h> // stat
 #include <fcntl.h> // open
 #include <unistd.h> // close
 
+#else
+#error Platform not supported
+#endif
+
 namespace FileUtils
 {
+#if defined _WIN32
+	bool Exists(const std::string & path)
+	{
+		DWORD fileAttributes;
+		try
+		{
+			fileAttributes = ::GetFileAttributes(path.c_str());
+		}
+		catch (...)
+		{
+			return false;
+		}
+		return (fileAttributes != INVALID_FILE_ATTRIBUTES);
+	}
+
+#undef CreateDirectory
+	bool CreateDirectory(const std::string & dirpath,
+		const unsigned int dirPerm)
+	{
+		return ::CreateDirectoryA(dirpath.c_str(), nullptr);
+	}
+
+#undef CreateFile
+	bool CreateFile(const std::string & filePath,
+		const unsigned int filePerm)
+	{
+		HANDLE result = ::CreateFileA(filePath.c_str(),
+			GENERIC_WRITE,
+			FILE_SHARE_READ,
+			nullptr,
+			CREATE_ALWAYS,
+			FILE_ATTRIBUTE_NORMAL,
+			nullptr);
+
+		if(result == INVALID_HANDLE_VALUE)
+		{
+			return false;
+		}
+
+		return ::CloseHandle(result);
+	}
+
+#elif defined __linux
 	// Path can be a directory or file
 	bool Exists(const std::string & path)
 	{
@@ -40,4 +90,5 @@ namespace FileUtils
 		close(fileDescriptor);
 		return true;
 	}
+#endif
 }
